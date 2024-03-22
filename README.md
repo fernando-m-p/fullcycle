b36db2da4dd9bb65661ee98bbf96e05285ca972a
FERNANDO MORAIS PEIXOTO <fernando.peixoto@trt22.jus.br>
2024-03-22 12:12:33 -0300

    feat: Código do Desafio 2 do módulo Docker

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# fullcycle-02-docker
-Repositório dos código do Curso FullCycle 3.0
+# Curso FullCycle 3.0
 
-Esse código é referente aos exemplos feitos na aula:
+## Módulo 2 - Docker (Desafio 01 - Go) 
 
+### O desafio foi fazer um projeto em que executando o comando de subir os containers suba:
+- Um container com um banco de dados
+- Um container com a aplicação Node/Javasript para mostrar: 
+- - <h1>Full Cycle Rocks!</h1>
+- - Lista de nomes cadastrada no banco de dados.
+- Um container com um Nginx como proxy reverso.
+
+
+### Diferenças do meu projeto:
+- Função para verificar se a tabela está criada e se não é criada a tabela;
+- Endpoint de cadastro de um novo registro no banco de dados;
+- Input e um link para acessar o endpoint de cadastro para o usuário criar um novo registro da página inicial;
+- Endpoint de deleção de um respectivo registro;
+- Link ao lado de cada nome cadastrado para excluir o registo.
+
+
+## Executando o projeto
 Para criar as imagens do node e do banco de dados
 ```bash
     docker compose build
@@ -21,13 +37,3 @@ ou
     docker-compose up
 ```
 
-
-Para subir o exemplo do laravel com o nginx na frente como proxy reverso:
-
-```bash
-    docker compose -f docker-compose-laravel.yml up
-```
-ou
-```bash
-    docker-compose -f docker-compose-laravel.yml  up
-```
--- node/index.js
+++ node/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const port = 3000
 const config = {
@@ -8,28 +10,30 @@ const config = {
     database: 'nodedb'
 
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `DROP TABLE IF EXISTS people;`
-connection.query(sql)
-
-const createTable = `CREATE TABLE people(id int not null auto_increment, name varchar(255), primary key(id));`
-connection.query(createTable)
-
-const names = ['Fernando', 'Morais', 'Peixoto']
-
-for (let i = 0; i < names.length; i++) {
-    connection.query('INSERT INTO people(name) values(?);', [names[i]])
-}
-connection.end(
-    function (err) {
-        if (err) throw err;
-        else console.log('Closing connection.')
-    });
 app.get('/', (req, res) => {
     readData(res)
 })
+
+app.get('/add', (req, res) => {
+    console.log(req.query);
+    if(req.query.nome){
+        add(req.query.nome)
+    }else{
+        add("Peixoto");
+    }
+    res.redirect('/')
+})
+
+app.get('/dell', (req, res) => {
+    console.log(req.query);
+    if(req.query.nome){
+        del(req.query.nome)
+    }
+    res.redirect('/')
+})
+createTable();
+
 
 function readData(res) {
     const connect = mysql.createConnection(config)
@@ -37,11 +41,16 @@ function readData(res) {
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
-            var html = '<h1>Full Cycle Rocks!</h1><br/>'
+            html = "<html><header></header><body>"
+            
+            var html = html+ '<h1>Full Cycle Rocks!</h1><br/>' +
+                '<p><input type="text" name="nome" id="name">'+
+                '<a class="btn btn-primary" onclick="nome = document.getElementById(\'name\').value;  const response = fetch(\'/add?nome=\'+nome);" href="/" role="button">Adicionar</a></p> <br/>';
             for (i = 0; i < results.length; i++) {
-                html += results[i].name + '<br/>';
+                html += results[i].name + '<a href="/dell?nome='+results[i].id+'" role="button"> X </a> <br/>';
             }
-            res.send(html);
+
+            res.send(html+"</body>");
         })
     connect.end(
         function (err) {
@@ -53,3 +62,41 @@ function readData(res) {
 app.listen(port, () => {
     console.log('rodando na porta' + port)
 })
+
+function createTable() {
+    const connection = mysql.createConnection(config)
+
+    const createTable = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key(id));`
+    connection.query(createTable)
+
+    connection.end(
+        function (err) {
+            if (err) throw err;
+            else console.log('Closing connection.')
+        });
+}
+
+function add(...names) {
+    const connection = mysql.createConnection(config)
+
+
+    for (let i = 0; i < names.length; i++) {
+        connection.query('INSERT INTO people(name) values(?);', [names[i]])
+    }
+    connection.end(
+        function (err) {
+            if (err) throw err;
+            else console.log('Closing connection.')
+        });
+}
+
+
+function del(name) {
+    const connection = mysql.createConnection(config)
+    connection.query('DELETE FROM nodedb.people WHERE id=?', [name])
+    connection.end(
+        function (err) {
+            if (err) throw err;
+            else console.log('Closing connection.')
+        });
+}
